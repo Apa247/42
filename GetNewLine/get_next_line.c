19f68889dff2607dbad42fb5602ed23ccdb213c2
David Aparicio gonzalez <daparici@c3r12s3.42madrid.com>
2022-04-21 21:04:39 +0200

    New Line

---
+++ GetNewLine/get_next_line.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.C                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daparici <daparici@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 20:14:28 by daparici          #+#    #+#             */
+/*   Updated: 2022/04/21 20:48:27 by daparici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	int		len;
+	char	*str;
+
+	while ()
+
+	len = read(fd, str, BUFFER_SIZE);
+
+	
+}
---
+++ GetNewLine/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daparici <daparici@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 20:14:41 by daparici          #+#    #+#             */
+/*   Updated: 2022/04/21 20:42:48 by daparici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <ctype.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <ctype.h>
+# include <xlocale.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+#endif
