597d2a2ce08d4a9e992efe6021bf4b56a825a20d
David Aparicio gonzalez <daparici@c3r14s1.42madrid.com>
2022-04-27 18:07:17 +0200

    GNL

--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -6,20 +6,32 @@
 /*   By: daparici <daparici@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 20:14:41 by daparici          #+#    #+#             */
-/*   Updated: 2022/04/21 20:42:48 by daparici         ###   ########.fr       */
+/*   Updated: 2022/04/27 16:05:06 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 # include <ctype.h>
 # include <stdio.h>
 # include <string.h>
 # include <unistd.h>
-# include <ctype.h>
 # include <xlocale.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# include <limits.h>  
+
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *res, char *copy);
+size_t	ft_strlen(char *str);
+char	*get_next_line(int fd);
+char	*copystr(int fd, char *res);
+char	*ft_getreserv(char *res);
+char	*ft_getline(char *res);
 
 #endif
